18ce1498516a677698c9c47fafe97781865f9dfc
Adrian Drabik <adriandrabik@gmail.com>
2019-01-13 18:18:56 +0100

    add ehcache

--- src/main/java/com/example/advancedhibernate/db/Post.java
+++ src/main/java/com/example/advancedhibernate/db/Post.java
@@ -1,15 +1,17 @@
 package com.example.advancedhibernate.db;
 
 import lombok.Data;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 
 @Data
 @Entity
 @Table(schema = "posting")
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Post {
     @SequenceGenerator(name = "post_id_gen", schema = "posting", sequenceName = "post_id_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_gen")
     @Id
     private Long id;
     @Version
---
+++ src/test/java/com/example/advancedhibernate/EhCacheSpec.groovy
@@ -0,0 +1,43 @@
+package com.example.advancedhibernate
+
+import com.example.advancedhibernate.db.Post
+import org.springframework.cache.CacheManager
+import spock.lang.Specification
+
+import javax.persistence.Cache
+import javax.persistence.Persistence
+import javax.persistence.Query
+
+class EhCacheSpec extends Specification {
+    def "check cache usage"() {
+        given:
+        def factory = Persistence.createEntityManagerFactory("jndi.ehcache.example")
+
+        Post post = new Post()
+        post.post = "Post"
+
+        when:
+        def entityManager = factory.createEntityManager()
+        entityManager.getTransaction().begin()
+        entityManager.persist(post)
+        entityManager.getTransaction().commit()
+
+        entityManager.getTransaction().begin()
+        def query = entityManager.createQuery('FROM Post WHERE id = :id')
+        query.setParameter("id", 1L)
+        query.setHint("org.hibernate.cacheable", true)
+        List<Post> found = query.getResultList() as List<Post>
+        entityManager.getTransaction().commit()
+
+        entityManager.getTransaction().begin()
+        def query2 = entityManager.createQuery('FROM Post WHERE id = :id')
+        query2.setParameter("id", 1L)
+        query2.setHint("org.hibernate.cacheable", true)
+        List<Post> found2 = query2.getResultList() as List<Post>
+        entityManager.getTransaction().commit()
+        def cache = factory.getCache()
+
+        then:
+        cache.contains(Post, post.id)
+    }
+}
